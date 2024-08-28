443a13d03bba6ca5b073bac622bf377b38451aea
Huynh Trieu <162791024+hhtrieu0108@users.noreply.github.com>
2024-08-28 09:45:24 +0700

    Time: 264 ms (34.43%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -1,21 +1,21 @@
 -- Write your PostgreSQL query statement below
 -- Solution 1
--- SELECT 
---     c.name as "Customers"
--- FROM 
---     Customers c LEFT JOIN Orders o on c.id = o.customerId
--- WHERE 
---     o.customerId IS NULL;
+SELECT 
+    c.name as "Customers"
+FROM 
+    Customers c LEFT JOIN Orders o on c.id = o.customerId
+WHERE 
+    o.customerId IS NULL;
 
 -- SOLUTION 2
-SELECT 
-    name AS Customers
-FROM 
-    Customers c
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM 
-        Orders o
-    WHERE 
-        o.customerId = c.id
-);
+-- SELECT 
+--     name AS Customers
+-- FROM 
+--     Customers c
+-- WHERE NOT EXISTS (
+--     SELECT 1
+--     FROM 
+--         Orders o
+--     WHERE 
+--         o.customerId = c.id
+-- );
