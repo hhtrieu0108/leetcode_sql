8e301e6e3aaa3aae8cb4a604fb7c00b216caa421
Huynh Trieu <162791024+hhtrieu0108@users.noreply.github.com>
2024-08-28 09:12:30 +0700

    Time: 247 ms (71.68%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,5 +1,6 @@
 -- Write your PostgreSQL query statement below
-SELECT e2.name as Employee
+SELECT 
+    e2.name as Employee
 FROM 
     Employee e1 INNER JOIN Employee e2 on e1.id = e2.managerId
 WHERE
